33bad9648026e48e1e362b3cdf343021803c8700
TMee3 <theopsna@protonmail.com>
2023-12-19 22:04:51 +0100

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATIC_URL = "/static/"
+STATIC_URL = "./static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "/static",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
